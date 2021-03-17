2c1414076280b7a153ac9f6e002faaf9318f9bff
kenodressel <hire@keno.digital>
2021-03-17 16:39:32 +0100

    chore: removes unused query

--- queries/followChain.ts
+++ queries/followChain.ts
@@ -17,23 +17,3 @@ SELECT *
 FROM subordinates
 ORDER BY height ASC;
 `;
-
-export const backwardQuery = (startHash: string) => `WITH RECURSIVE subordinates AS (
-    SELECT "keyHash",
-           "lastKeyHash",
-           "timestamp",
-           "height"
-    FROM "Blocks"
-    WHERE "keyHash" = '${startHash}' -- start
-    UNION
-    SELECT b."keyHash",
-           b."lastKeyHash",
-           b.timestamp,
-           b.height
-    FROM "Blocks" b
-             INNER JOIN subordinates s ON s."lastKeyHash" = b."keyHash"
-)
-SELECT *
-FROM subordinates
-ORDER BY height ASC;
-`;
